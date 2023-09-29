7d5830142dc84c6904e4c0b85bf990e78e41095f
DIALLO <ousmane.diallo1.sio@gmail.com>
2023-09-29 22:20:02 +0200

    front/feat/base-navigation

--- front-end/src/index.tsx
+++ front-end/src/index.tsx
@@ -3,13 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Router from './router';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Router />
+    </BrowserRouter>
+    {/* <App /> */}
   </React.StrictMode>
 );
 
