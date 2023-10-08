e65e67213230141256c3e77f4e71b36e60b4c681
Aaldn <aaldn.dev@gmail.com>
2023-10-08 22:39:47 +0200

    feat: cors

--- back-end/src/index.ts
+++ back-end/src/index.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import userController from './Users/UserController';
+import cors from "cors";
 
 const app = express()
+app.use("*", cors())
 
 app.use(bodyParser.json())
 app.use('/user', userController)
