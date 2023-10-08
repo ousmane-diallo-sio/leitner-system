c7e5b8832d43b7bd5fd81ae30d2966cfe2de0079
BlackBenE <120026521+BlackBenE@users.noreply.github.com>
2023-10-09 00:02:11 +0200

    Update Sheetcontroller

--- back-end/src/Sheet/SheetController.ts
+++ back-end/src/Sheet/SheetController.ts
@@ -1,12 +1,30 @@
 const express = require('express');
 const router = express.Router();
-const sheetServices = require('./SheetService'); 
+const sheetServices = require('./SheetService');
 
-router.post('/sheets', sheetServices.createSheet);
-router.post('/sheets/questionnaire', sheetServices.triggerQuestionnaire);
-router.get('/sheets', sheetServices.getUserSheets);
-router.put('/sheets/:sheetId/validate', sheetServices.validateSheet);
-router.get('/sheets/tags/:tag', sheetServices.getSheetsByTag);
-router.post('/sheets/notify', sheetServices.setNotification);
+router.post('/sheets', (req: any, res: any) => {
+    const newSheet = req.body;
+    sheetServices.createSheet(newSheet);
+    res.status(201).json({ message: 'Feuille créée avec succès' });
+});
+
+router.put('/sheets/:sheetId/updateCategory', (req: any, res: any) => {
+    const sheetId = req.params.sheetId;
+    const userAnswer = req.body.userAnswer;
+
+    try {
+        const isCorrect = sheetServices.compareAnswers(sheetId, userAnswer);
+
+        if (isCorrect) {
+           
+            sheetServices.updateSheetCategory(sheetId, '1'); 
+            res.status(200).json({ message: 'Réponse correcte, catégorie mise à jour' });
+        } else {
+            res.status(200).json({ message: 'Réponse incorrecte' });
+        }
+    } catch (error) {
+        res.status(404).json({ message: 'Feuille non trouvée' });
+    }
+});
 
 module.exports = router;
