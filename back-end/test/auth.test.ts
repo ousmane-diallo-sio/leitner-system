c7ab00ba98c65d7c988b98baf944abff04948a07
Aaldn <aaldn.dev@gmail.com>
2023-10-08 19:51:47 +0200

    test: change expected response

--- back-end/test/auth.test.ts
+++ back-end/test/auth.test.ts
@@ -8,7 +8,7 @@ describe('authentication', () => {
       .post('/user/login')
       .send({ username: 'ValidUsername', password: 'ValidPassword' });
     expect(response.status).toBe(200);
-    expect(response.body.auth).toBe(true);
+    expect(response.body).not.toBeNull();
   });
 
   test('should return auth false for valid username and invalid password', async () => {
@@ -16,7 +16,7 @@ describe('authentication', () => {
       .post('/user/login')
       .send({ username: 'ValidUsername', password: 'InvalidPassword' });
     expect(response.status).toBe(401);
-    expect(response.body.auth).toBe(false);
+    expect(response.body).toBeNull();
   });
 
   test('should return auth false for invalid username and valid password', async () => {
@@ -24,7 +24,7 @@ describe('authentication', () => {
       .post('/user/login')
       .send({ username: 'InvalidUsername', password: 'ValidPassword' });
     expect(response.status).toBe(401);
-    expect(response.body.auth).toBe(false);
+    expect(response.body).toBeNull();
   });
 
   test('should return auth false for invalid username and invalid password', async () => {
@@ -32,6 +32,6 @@ describe('authentication', () => {
       .post('/user/login')
       .send({ username: 'InvalidUsername', password: 'InvalidPassword' });
     expect(response.status).toBe(401);
-    expect(response.body.auth).toBe(false);
+    expect(response.body).toBeNull();
   });
 });
