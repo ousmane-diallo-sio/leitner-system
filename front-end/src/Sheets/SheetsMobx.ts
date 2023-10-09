6251e4b57ee09c5e302562e6430419f46de9309d
DIALLO <ousmane.diallo1.sio@gmail.com>
2023-10-10 01:46:17 +0200

    feat: sheets mobx and getSheets API call

--- front-end/src/Sheets/SheetsMobx.ts
+++ front-end/src/Sheets/SheetsMobx.ts
@@ -1,1 +1,31 @@
-export {}
+import { action, makeAutoObservable, observable } from "mobx"
+import { publicRoutes } from "../router"
+import { create, persist } from "mobx-persist"
+
+let instance: Sheet
+export class Sheet {
+
+  constructor() {
+    if (instance) {
+      makeAutoObservable(this)
+      return instance
+    }
+    instance = this
+    makeAutoObservable(this)
+  }
+
+  @persist("object")
+  @observable sheets: SheetType[] = []
+
+  @action.bound
+  setSheets(sheets: SheetType[]) {
+    this.sheets = sheets
+  }
+
+}
+
+const hydrate = create({ storage: localStorage })
+const sheetMobx = new Sheet()
+hydrate("user", sheetMobx)
+
+export default sheetMobx
--- front-end/src/Sheets/SheetsService.ts
+++ front-end/src/Sheets/SheetsService.ts
@@ -1,5 +1,7 @@
 import axios from 'axios';
 import envUtils from '../utils/EnvUtils';
+import sheetMobx from './SheetsMobx';
+import toast from 'react-hot-toast';
 
 class SheetsService {
 
@@ -12,7 +14,25 @@ class SheetsService {
   })
 
   async getSheets() {
-    return null
+    try {
+      const res = await this.axiosClient.post("/sheets")
+      const resData = res.data as ServerResponse<SheetType[] |null>
+
+      if (res.status === 200 && resData.data) {
+        sheetMobx.setSheets(resData.data)
+        return resData.data
+      }
+
+      if (resData.error) {
+        toast.error(resData.error)
+      }
+
+      return null
+    } catch (error) {
+      toast.error("Erreur lors du chargement des questions")
+      console.log("error: ", error)
+      return null
+    }
   }
 }
 
